f8343353c1bf4dc74fae6085378a4cf4043b53c0
Md Rayhan <mohammadrayhan796@gmail.com>
2026-01-04 18:06:08 +0600

    added js file and chekc the js file

--- script/index.js
+++ script/index.js
@@ -2,3 +2,5 @@
 // document.getElementById('card-btn-steel').addEventListener("click",function(){
 //     alert('STEEL ER HARI CLICKED')
 // })
+
+const titles = document.getElementsByClassName('card-title');
