f46a44ab4bb23fdf94c0a0e1254610520834061c
Md Rayhan <mohammadrayhan796@gmail.com>
2026-01-12 16:31:23 +0600

    update porchase cart section

--- script/index.js
+++ script/index.js
@@ -9,8 +9,8 @@
 //    title.classList.remove("card-title")
 // }
 
-// 
-const images = document.getElementsByTagName('img');
+// // 
+// const images = document.getElementsByTagName('img');
 
 // for (let img of images) {
 //  img.addEventListener('mouseover',function(){
@@ -19,13 +19,42 @@ const images = document.getElementsByTagName('img');
 // }
 
 function getElement(id){
-    const element = document.getElementById(id);
-    return element;
+    return document.getElementById(id);
 }
-document.getElementById('card-btn-1').addEventListener("click",function()
-{
-    const title =getElement('card-title-1').innerText;
-    const price = getElement('card-price-1').innerText;
-  
-    
-})
+
+document.getElementById('card-btn-1').addEventListener("click", function () {
+
+    const title = getElement('card-title-1').innerText;
+
+    const price = Number(getElement('card-price-1').innerText);
+
+    const totalprice = Number(getElement('total-price').innerText);
+
+    const currentTotal = price + totalprice;
+
+    getElement('total-price').innerText = currentTotal.toFixed(2);
+
+    const cartContainer = getElement('cart-container');
+
+const newCart = document.createElement("div");
+
+newCart.innerHTML = `
+
+                  <div class="bg-gray-500 rounded-xl flex justify-between
+                  p-4 mb-5">
+                  <img src="./assets/kitchen-1.png" alt="" class="w-10">
+                  <div class="" >
+                    <h2>Product Name</h2>
+                    <p>Price : 39 tk</p>
+                  </div>
+
+                  </div>
+              
+`;
+cartContainer.append(newCart);
+});
+
+
+
+
+
